7552fc56402ac5ab7b8f54ad6e25a030aeae95c7
lina <61802332+li-na-k@users.noreply.github.com>
2023-01-31 14:20:14 +0100

    itialize project with spring initializr

---
+++ src/main/java/com/example/distanceCalc/DistanceCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.distanceCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/distanceCalc/DistanceCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distanceCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
